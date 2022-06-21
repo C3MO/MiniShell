9c65e330b06d1bd56c7965360b4487851642ccaf
Cem Saygili <c.saygili@prokommunal.com>
2022-06-21 14:51:17 +0200

    [fix] change and adapt codestyle
    - add gitignore
    - add documentation for method

--- MShell.java
+++ MShell.java
@@ -1,19 +1,20 @@
-import java.io.*;
-import java.util.*;
-
-/**
- * Created by Cem Saygili, MTK: 87707 on 13.06.18.
- */
-public class MShell
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
-{
+public class MShell {
 
+    /**
+     * The function takes in a command from the user and executes it
+     */
     public static void main(String[] args) throws java.io.IOException {
         String command;
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-System.out.println("***** MiniShell *****");
+        System.out.println("***** MiniShell *****");
         while (true) {
 
             System.out.print(">");
@@ -31,8 +32,9 @@ System.out.println("***** MiniShell *****");
                     System.exit(0);
 
                 }
+                // This is the run command. It is taking in the command and executing it.
                 if (command.equals("run")) {
-                    if ((args.length <= 0) && (args[1].equals( "-l"))) {
+                    if ((args.length <= 0) && (args[1].equals("-l"))) {
 
                         System.err.println("Need command to run");
                         System.exit(-1);
@@ -48,13 +50,17 @@ System.out.println("***** MiniShell *****");
                     System.out.printf("Output of running %s is:\n",
                             Arrays.toString(args));
 
+                    // This is taking the command that the user has entered and executing it.
                     while ((line = br.readLine()) != null) {
                         System.out.println(line);
                     }
                     continue;
                 }
+                // This is the parallel command. It is taking in the command and executing it.
                 if (command.equals("parallel")) {
-                    if ((args.length <= 0) && (args[2].equals( "date"))) {
+                    // This is checking if the user has entered a command to run. If they have not entered a command, it
+                    // will print out an error message and exit the program.
+                    if ((args.length <= 0) && (args[2].equals("date"))) {
 
                         System.err.println("Need command to run");
                         System.exit(-1);
@@ -68,49 +74,41 @@ System.out.println("***** MiniShell *****");
                     String line;
 
                     System.out.printf("Output of running %s is:\n",
-                          Arrays.toString(args));
+                            Arrays.toString(args));
 
+                    // This is taking the command that the user has entered and executing it.
                     while ((line = br.readLine()) != null) {
-                    String [] tokens = line.split(" ");
-                    System.out.println(Arrays.toString(tokens));
+                        String[] tokens = line.split(" ");
+                        System.out.println(Arrays.toString(tokens));
 
                     }
                     continue;
                 }
 
-                    ArrayList<String> parms = new ArrayList<String>();
-
-                    String[] lineSplit = command.split(" ");
+                String[] lineSplit = command.split(" ");
 
 
-                    int size = lineSplit.length;
-
-                    for (int i = 0; i < size; i++) {
-
-                        parms.add(lineSplit[i]);
+                ArrayList<String> parms = new ArrayList<>(Arrays.asList(lineSplit));
 
 
-                    }
+                ProcessBuilder pb = new ProcessBuilder(parms);
+                Process proc = pb.start();
 
+                InputStream is = proc.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
 
-                    ProcessBuilder pb = new ProcessBuilder(parms);
-                    Process proc = pb.start();
+                String line;
+                while ((line = br.readLine()) != null)
+                    System.out.println(line);
+                br.close();
 
-                    InputStream is = proc.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-
-                    String line;
-                    while ((line = br.readLine()) != null)
-                        System.out.println(line);
-                    br.close();
-
-
-                }
 
             }
 
         }
 
     }
+
+}
 
