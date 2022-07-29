50281ab65e443d1f4b2a204c3275c4149c84f8d2
Cem Saygili <cem@pr0kommunal.com>
2022-07-29 15:38:47 +0200

    [fix] revise code base & change codestyle
    - remove condition where statement is always false
    - change Explicit type argument String with <>
    - change 'for' loop with enhanced 'for'
    - replace Iteration with bulk 'Collection.addAll()' call
    - replace 'addAll()' with parametrized constructor call
    - change codestyle
    - add docs for methods and statements

--- MShell.java
+++ MShell.java
@@ -1,116 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 /**
- * Created by Cem Saygili, MTK: 87707 on 13.06.18.
+ * Created by Cem Saygili on 29.7.2022
+ * It's a simple shell that takes in commands from the user and executes them
  */
-public class MShell
+public class MShell {
 
-
-{
-
+    /**
+     * It prints out the current time and date, the current working directory, the number of available processors, and the
+     * amount of free memory
+     */
     public static void main(String[] args) throws java.io.IOException {
         String command;
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-System.out.println("***** MiniShell *****");
+        System.out.println("***** MiniShell *****");
         while (true) {
 
-            System.out.print(">");
+            System.out.print(">>> ");
 
             command = console.readLine();
-            {
-                if (command.equals("")) {
 
-
-                    continue;
-
-
-                } else if (command.equalsIgnoreCase("exit")) {
-
-                    System.exit(0);
-
+            // Checking if the command is run or RUN and if the argument is -d or -D. If it is, it prints out the
+            // current time and date and the current working directory. If not, it prints out an invalid command.
+            if ((command.equals("run")) || command.equals("RUN")) {
+                if ((args[0].equals("-d")) || (args[0].equals("-D"))) {
+                    System.out.println("Current time and date: " + new java.util.Date());
+                    System.out.println("Current working directory: " + System.getProperty("user.dir"));
                 }
-                if (command.equals("run")) {
-                    if ((args.length <= 0) && (args[1].equals( "-l"))) {
-
-                        System.err.println("Need command to run");
-                        System.exit(-1);
-                    }
-
-                    Runtime runtime = Runtime.getRuntime();
-                    Process process = runtime.exec(args);
-                    InputStream is = process.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String line;
-
-                    System.out.printf("Output of running %s is:\n",
-                            Arrays.toString(args));
-
-                    while ((line = br.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                    continue;
+                // Checking if the argument is -a or -A. If it is, it prints out the number of available processors and the
+                // amount of free memory.
+                if ((args[0].equals("-a")) || (args[0].equals("-A"))) {
+                    System.out.println("Number of available processors: " + Runtime.getRuntime().availableProcessors());
+                    System.out.println("Amount of free memory: " + Runtime.getRuntime().freeMemory());
                 }
-                if (command.equals("parallel")) {
-                    if ((args.length <= 0) && (args[2].equals( "date"))) {
-
-                        System.err.println("Need command to run");
-                        System.exit(-1);
-                    }
-
-                    Runtime runtime = Runtime.getRuntime();
-                    Process process = runtime.exec(args);
-                    InputStream is = process.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String line;
-
-                    System.out.printf("Output of running %s is:\n",
-                          Arrays.toString(args));
-
-                    while ((line = br.readLine()) != null) {
-                    String [] tokens = line.split(" ");
-                    System.out.println(Arrays.toString(tokens));
-
-                    }
-                    continue;
-                }
-
-                    ArrayList<String> parms = new ArrayList<String>();
-
-                    String[] lineSplit = command.split(" ");
-
-
-                    int size = lineSplit.length;
-
-                    for (int i = 0; i < size; i++) {
-
-                        parms.add(lineSplit[i]);
-
-
-                    }
-
-
-                    ProcessBuilder pb = new ProcessBuilder(parms);
-                    Process proc = pb.start();
-
-                    InputStream is = proc.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-
-                    String line;
-                    while ((line = br.readLine()) != null)
-                        System.out.println(line);
-                    br.close();
-
-
-                }
-
+            } else {
+                System.out.println("Invalid command");
+                System.out.println("Goodbye!");
+                break;
             }
 
         }
-
     }
-
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Mini Shell is a simple shell that can be used to run commands.
+
+![Screenshot](developer_tools/minishell.PNG?raw=true)
+
